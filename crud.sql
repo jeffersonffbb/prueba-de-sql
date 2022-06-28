45bbda7d7a461cb4fbd863997dcdc28afa1856d1
jeffersonffbb <105877310+jeffersonffbb@users.noreply.github.com>
2022-06-28 12:52:40 -0400

    se crea un crud.sql
    se crea 3 tablas y se usan 2 foreign key , se hace uso a las primary key, al auto increment con los id,se seleccionas las tablas, se miran las base de datos, las tablas etc

---
+++ crud.sql
@@ -0,0 +1,80 @@
+#ver las bases de datos
+
+show databases;
+
+#usar el base de dato colegio
+
+use colegio;
+
+#crear una tabla
+
+create table director(
+id int not null,
+nombre char(120) not null,
+correo varchar (120) not null,
+PRIMARY KEY (id)
+);#definimos nuestra primary key
+
+#alteramos la tabla para que valla de formar auto incrementado en base a lo que se ingresa
+
+alter table director modify column id int auto_increment;
+
+#insertamos los datos correspondientes
+
+insert into director (nombre,correo) values
+	('Patricio Carrazco','nose1234@gmail.com'),
+	('luz','luz4567@gmail.com'),
+	('marco','mar@gmail.com'),
+	('christofer','christofer@gmail.com');
+    
+#seleccionamos la tabla director para que muestre lo que se inserto
+
+select * from director;
+
+#selecciona la tabla director y el id 3 de forma especifica
+
+select* from director where id =3;
+
+#crear la segunda tabla
+
+create table profesor(
+id int not null,
+nombre varchar(120) not null,
+edad int not null,
+trabaja_para int,
+PRIMARY KEY (id),
+Foreign key (trabaja_para) references director(id)
+);#definimos la primary key y foreign key en base al id del director
+
+alter table profesor modify column id int auto_increment;
+
+insert into profesor (nombre,edad,trabaja_para) values 
+	('jeff','34','4'),
+	('luz','30','1'),
+	('marco','23','2'),
+	('christofer','35','2');
+    
+#volvemos a seleccionar el profesor para verificar nuestros datos ingresados    
+    
+select * from profesor;
+show tables;
+
+create table inspector(
+id int not null,
+nombre char(120) not null,
+correo varchar (120)not null,
+le_paga_el int,
+PRIMARY KEY (id),
+Foreign key (le_paga_el) references director(id)
+);
+
+alter table inspector modify column id int auto_increment;
+insert into inspector (nombre,correo,le_paga_el) values 
+	('jeff','jeffersonvisitor23@gmail.com','2'),
+	('luz','luznoseque@gmail.com','3'),
+	('alex','alex21@gmail.com','1'),
+	('alfredo','alfredo@gmail.com','4');
+    
+select * from inspector;
+
+select* from inspector where id =3;
